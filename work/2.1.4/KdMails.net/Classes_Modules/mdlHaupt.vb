cda87048035de25de93f1732a811549cce2953dc
DefStevo@gmail.com <DefStevo@gmail.com@1bb4064d-4001-7b1c-8927-4fcd3d59ee48>
2013-04-16 18:02:00 +0000

    [KdMails.net] v 2.1.4 ----------------------------- Arbeitskopie

---
+++ work/2.1.4/KdMails.net/Classes_Modules/mdlHaupt.vb
@@ -0,0 +1,145 @@
+﻿#Region "Imports"
+Imports Microsoft.Win32
+
+#End Region
+
+Module mdlHaupt
+
+#Region "Deklartionen"
+    Private _strApp_Pfad As String = Application.ExecutablePath.Substring(0, Application.ExecutablePath.LastIndexOf("\"))
+    Private _strUpdate_Pfad As String = ""
+    Private _strVersion_Lokal As String = My.Application.Info.Version.Major.ToString & "." & _
+                                          My.Application.Info.Version.Minor.ToString & "." & _
+                                          My.Application.Info.Version.Build.ToString & "." & _
+                                          My.Application.Info.Version.Revision.ToString
+    Private _strVersion_Remote As String = ""
+    Private _strVersion_Update_Lokal As String = ""
+    Private _strVersion_Update_Remote As String = ""
+    Public _FolderBrowser As FolderBrowserDialog
+
+#End Region
+
+#Region "Sub/Funktionen"
+    Sub Main()
+        _FolderBrowser = New FolderBrowserDialog
+        _FolderBrowser.RootFolder = Environment.SpecialFolder.MyComputer
+
+        If Not Auto_Update() = True Then
+
+            For i As Integer = 0 To My.Application.CommandLineArgs.Count - 1
+                Select Case My.Application.CommandLineArgs(i)
+                    Case "/Neu"
+                        'Prüfen ob Update.log existiert
+                        If System.IO.File.Exists(_strUpdate_Pfad + "\Update.log") Then
+                            FileCopy(_strUpdate_Pfad + "\Update.log", _
+                                     _strApp_Pfad + "\Update.log")
+                        End If
+
+                        If System.IO.File.Exists(_strApp_Pfad + "\Update.log") Then
+                            frmHistorie.ShowDialog()
+                        End If
+
+                End Select
+            Next
+
+
+            frmHaupt.ShowDialog()
+        End If
+    End Sub
+
+    Function Auto_Update() As Boolean
+        If Registry.CurrentUser.OpenSubKey("Software").OpenSubKey("KdMails.net") Is Nothing Then
+            Registry.CurrentUser.OpenSubKey("Software", True).CreateSubKey("KdMails.net")
+        End If
+
+        Lese_Registry("Update_Pfad", _strUpdate_Pfad)
+
+        'Update Pfad noch nicht definiert
+        If _strUpdate_Pfad = "" Then
+            If MsgBox("Update Verzeichnis noch nicht definiert" & vbNewLine & "Wollen Sie dies auswählen?", MsgBoxStyle.YesNo, "KdMails.net Auto Update") = vbYes Then
+                _FolderBrowser.ShowDialog()
+                _strUpdate_Pfad = _FolderBrowser.SelectedPath
+                Schreibe_Registry("Update_Pfad", _strUpdate_Pfad)
+
+            Else
+                'Update Pfad nicht eingestellt
+                Return False
+            End If
+
+        End If
+
+        'Prüfen Version von Update.exe
+        _strVersion_Update_Lokal = "0"
+        _strVersion_Update_Remote = "0"
+
+        If System.IO.File.Exists(_strApp_Pfad + "\KdMails.net.Update.exe") Then
+            _strVersion_Update_Lokal = FileVersionInfo.GetVersionInfo(_strApp_Pfad + "\KdMails.net.Update.exe").FileVersion.ToString
+        End If
+
+        If System.IO.File.Exists(_strUpdate_Pfad + "\KdMails.net.Update.exe") Then
+            _strVersion_Update_Remote = FileVersionInfo.GetVersionInfo(_strUpdate_Pfad + "\KdMails.net.Update.exe").FileVersion.ToString
+        End If
+
+        'Falls Version unterschiedlich Update durchführen
+        If Not _strVersion_Update_Remote = "0" And Not _strVersion_Update_Lokal = _strVersion_Update_Remote Then
+            'Sicherungskopie löschen
+            If System.IO.File.Exists(_strApp_Pfad + "\KdMails.net.Update.exe.alt") Then
+                System.IO.File.Delete(_strApp_Pfad + "\KdMails.net.Update.exe.alt")
+            End If
+            'Sicherungkopie erstellen
+            If System.IO.File.Exists(_strApp_Pfad + "\KdMails.net.Update.exe") Then
+                System.IO.File.Move(_strApp_Pfad + "\KdMails.net.Update.exe", _strApp_Pfad + "\KdMails.net.Update.exe.alt")
+            End If
+
+            FileCopy(_strUpdate_Pfad + "\KdMails.net.Update.exe", _
+                     _strApp_Pfad + "\KdMails.net.Update.exe")
+        End If
+
+        'Prüfen Version von KdMails.net()
+        _strVersion_Remote = "0"
+
+        If System.IO.File.Exists(_strUpdate_Pfad + "\KdMails.net.exe") Then
+            _strVersion_Remote = FileVersionInfo.GetVersionInfo(_strUpdate_Pfad + "\KdMails.net.exe").FileVersion.ToString
+        End If
+
+        'Falls Versoin unterschiedlich Update durchführen
+        If Not _strVersion_Remote = "0" And Not _strVersion_Lokal = _strVersion_Remote Then
+
+            If MsgBox("Es steht ein Update zur Verfügung!!!" & vbNewLine & _
+                      "Aktuelle Version: " & _strVersion_Lokal & vbNewLine & _
+                      "Neue Version: " & _strVersion_Remote & " (" & _strUpdate_Pfad & ")" & vbNewLine & _
+                      vbNewLine & _
+                      "Wollen Sie dies verwenden?", MsgBoxStyle.YesNo, "KdMails.net Auto Update") = vbYes Then
+
+                Dim strCommand As String = ""
+
+                strCommand = " /RDatei=""" & _strUpdate_Pfad & "\KdMails.net.exe""" & _
+                           " /LDatei=""" & _strApp_Pfad & "\KdMails.net.exe""" & _
+                           " /RVersion=""" & _strVersion_Remote & """" & _
+                           " /LVersion=""" & _strVersion_Lokal & """"
+
+                Shell(_strApp_Pfad + "\KdMails.net.Update.exe" + strCommand, AppWinStyle.NormalFocus, False)
+
+                'Funktion mit True beenden
+                Return True
+            Else
+                Return False
+            End If
+
+        End If
+
+    End Function
+
+#End Region
+
+#Region "Registry"
+    Public Sub Lese_Registry(ByVal name As String, ByRef Wert As String)
+        Wert = Registry.CurrentUser.OpenSubKey("Software").OpenSubKey("KdMails.net").GetValue("Update_Pfad", "")
+    End Sub
+
+    Public Sub Schreibe_Registry(ByVal Name As String, ByVal Wert As String)
+        Registry.CurrentUser.OpenSubKey("Software").OpenSubKey("KdMails.net", True).SetValue(Name, Wert)
+    End Sub
+#End Region
+
+End Module
---
+++ work/2.1.4/KdMails.net/Forms/frmHistorie.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmHistorie
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmHistorie))
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.txtDatum = New System.Windows.Forms.TextBox()
+        Me.btnZuruck = New System.Windows.Forms.Button()
+        Me.btnVor = New System.Windows.Forms.Button()
+        Me.btnAnfang = New System.Windows.Forms.Button()
+        Me.btnEnde = New System.Windows.Forms.Button()
+        Me.txtVersion = New System.Windows.Forms.TextBox()
+        Me.txtAnderung = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(425, 3)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(75, 23)
+        Me.btnOK.TabIndex = 0
+        Me.btnOK.Text = "OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'txtDatum
+        '
+        Me.txtDatum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtDatum.Location = New System.Drawing.Point(161, 3)
+        Me.txtDatum.Name = "txtDatum"
+        Me.txtDatum.ReadOnly = True
+        Me.txtDatum.Size = New System.Drawing.Size(67, 22)
+        Me.txtDatum.TabIndex = 1
+        '
+        'btnZuruck
+        '
+        Me.btnZuruck.Enabled = False
+        Me.btnZuruck.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnZuruck.Image = Global.KdMails.net.My.Resources.Resources.Zuruck
+        Me.btnZuruck.Location = New System.Drawing.Point(27, 3)
+        Me.btnZuruck.Name = "btnZuruck"
+        Me.btnZuruck.Size = New System.Drawing.Size(15, 20)
+        Me.btnZuruck.TabIndex = 2
+        Me.btnZuruck.UseVisualStyleBackColor = True
+        '
+        'btnVor
+        '
+        Me.btnVor.Enabled = False
+        Me.btnVor.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnVor.Image = Global.KdMails.net.My.Resources.Resources.Vor
+        Me.btnVor.Location = New System.Drawing.Point(45, 3)
+        Me.btnVor.Name = "btnVor"
+        Me.btnVor.Size = New System.Drawing.Size(15, 20)
+        Me.btnVor.TabIndex = 3
+        Me.btnVor.UseVisualStyleBackColor = True
+        '
+        'btnAnfang
+        '
+        Me.btnAnfang.Enabled = False
+        Me.btnAnfang.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnAnfang.Image = Global.KdMails.net.My.Resources.Resources.Anfang
+        Me.btnAnfang.Location = New System.Drawing.Point(1, 3)
+        Me.btnAnfang.Name = "btnAnfang"
+        Me.btnAnfang.Size = New System.Drawing.Size(25, 20)
+        Me.btnAnfang.TabIndex = 4
+        Me.btnAnfang.UseVisualStyleBackColor = True
+        '
+        'btnEnde
+        '
+        Me.btnEnde.Enabled = False
+        Me.btnEnde.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnEnde.Image = Global.KdMails.net.My.Resources.Resources.Ende
+        Me.btnEnde.Location = New System.Drawing.Point(61, 3)
+        Me.btnEnde.Name = "btnEnde"
+        Me.btnEnde.Size = New System.Drawing.Size(25, 20)
+        Me.btnEnde.TabIndex = 5
+        Me.btnEnde.UseVisualStyleBackColor = True
+        '
+        'txtVersion
+        '
+        Me.txtVersion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtVersion.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtVersion.Location = New System.Drawing.Point(101, 3)
+        Me.txtVersion.Name = "txtVersion"
+        Me.txtVersion.ReadOnly = True
+        Me.txtVersion.Size = New System.Drawing.Size(54, 22)
+        Me.txtVersion.TabIndex = 6
+        '
+        'txtAnderung
+        '
+        Me.txtAnderung.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtAnderung.Location = New System.Drawing.Point(1, 28)
+        Me.txtAnderung.Multiline = True
+        Me.txtAnderung.Name = "txtAnderung"
+        Me.txtAnderung.ReadOnly = True
+        Me.txtAnderung.Size = New System.Drawing.Size(499, 221)
+        Me.txtAnderung.TabIndex = 7
+        '
+        'frmHistorie
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(502, 251)
+        Me.Controls.Add(Me.txtAnderung)
+        Me.Controls.Add(Me.txtVersion)
+        Me.Controls.Add(Me.btnEnde)
+        Me.Controls.Add(Me.btnAnfang)
+        Me.Controls.Add(Me.btnVor)
+        Me.Controls.Add(Me.btnZuruck)
+        Me.Controls.Add(Me.txtDatum)
+        Me.Controls.Add(Me.btnOK)
+        Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmHistorie"
+        Me.Text = "KdMails.net - Kundenmails kopieren - Änderungshistorie"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnOK As System.Windows.Forms.Button
+    Friend WithEvents txtDatum As System.Windows.Forms.TextBox
+    Friend WithEvents btnZuruck As System.Windows.Forms.Button
+    Friend WithEvents btnVor As System.Windows.Forms.Button
+    Friend WithEvents btnAnfang As System.Windows.Forms.Button
+    Friend WithEvents btnEnde As System.Windows.Forms.Button
+    Friend WithEvents txtVersion As System.Windows.Forms.TextBox
+    Friend WithEvents txtAnderung As System.Windows.Forms.TextBox
+End Class
