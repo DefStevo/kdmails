0a8890a40dde114ff5423e5387b019bfef925ba9
DefStevo@gmail.com <DefStevo@gmail.com@1bb4064d-4001-7b1c-8927-4fcd3d59ee48>
2015-03-03 12:20:15 +0000

    [KdMails.net] v 2.2.0 ----------------------------- Arbeitskopie

---
+++ work/2.2.0/2.1.4/KdMails.net/Forms/frmMails.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMails
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyleCheckBoxEnabled As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyleCheckBoxDisabled As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMails))
+        Me.dgMails = New System.Windows.Forms.DataGridView()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnAbbrechen = New System.Windows.Forms.Button()
+        Me.btnKopieren = New System.Windows.Forms.Button()
+        Me.cEA = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.cMail = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.cMailEID = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.cOrdner = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.cOrdnerEID = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.cOrdnerSID = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.cCopy = New System.Windows.Forms.DataGridViewCheckBoxColumn()
+        Me.cIgnore = New System.Windows.Forms.DataGridViewCheckBoxColumn()
+        Me.cMailAdress = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.dgMails, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'dgMails
+        '
+        Me.dgMails.AllowUserToAddRows = False
+        Me.dgMails.AllowUserToDeleteRows = False
+        Me.dgMails.AllowUserToOrderColumns = True
+        Me.dgMails.AllowUserToResizeRows = False
+        Me.dgMails.BackgroundColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(241, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.dgMails.ColumnHeadersHeight = 20
+        Me.dgMails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+        Me.dgMails.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.cEA, Me.cMail, Me.cMailEID, Me.cOrdner, Me.cOrdnerEID, Me.cOrdnerSID, Me.cCopy, Me.cIgnore, Me.cMailAdress})
+        Me.dgMails.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgMails.Location = New System.Drawing.Point(0, 0)
+        Me.dgMails.MultiSelect = False
+        Me.dgMails.Name = "dgMails"
+        Me.dgMails.RowHeadersWidth = 15
+        Me.dgMails.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing
+        Me.dgMails.RowTemplate.Height = 30
+        Me.dgMails.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.dgMails.ShowCellToolTips = False
+        Me.dgMails.ShowEditingIcon = False
+        Me.dgMails.Size = New System.Drawing.Size(873, 405)
+        Me.dgMails.TabIndex = 1
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.btnAbbrechen)
+        Me.Panel1.Controls.Add(Me.btnKopieren)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel1.Location = New System.Drawing.Point(0, 405)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(873, 20)
+        Me.Panel1.TabIndex = 2
+        '
+        'btnAbbrechen
+        '
+        Me.btnAbbrechen.Dock = System.Windows.Forms.DockStyle.Right
+        Me.btnAbbrechen.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnAbbrechen.Location = New System.Drawing.Point(573, 0)
+        Me.btnAbbrechen.Name = "btnAbbrechen"
+        Me.btnAbbrechen.Size = New System.Drawing.Size(300, 20)
+        Me.btnAbbrechen.TabIndex = 5
+        Me.btnAbbrechen.Text = "A&bbrechen"
+        Me.btnAbbrechen.UseVisualStyleBackColor = True
+        '
+        'btnKopieren
+        '
+        Me.btnKopieren.Dock = System.Windows.Forms.DockStyle.Left
+        Me.btnKopieren.Enabled = False
+        Me.btnKopieren.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnKopieren.Location = New System.Drawing.Point(0, 0)
+        Me.btnKopieren.Name = "btnKopieren"
+        Me.btnKopieren.Size = New System.Drawing.Size(300, 20)
+        Me.btnKopieren.TabIndex = 4
+        Me.btnKopieren.Text = "&Kopieren"
+        Me.btnKopieren.UseVisualStyleBackColor = True
+        '
+        'cEA
+        '
+        Me.cEA.HeaderText = "EA"
+        Me.cEA.Name = "cEA"
+        Me.cEA.ReadOnly = True
+        Me.cEA.Width = 25
+        '
+        'cMail
+        '
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.cMail.DefaultCellStyle = DataGridViewCellStyle1
+        Me.cMail.HeaderText = "E-Mail"
+        Me.cMail.Name = "cMail"
+        Me.cMail.ReadOnly = True
+        Me.cMail.ToolTipText = "Doppelklick für Mail Informationen"
+        Me.cMail.Width = 400
+        '
+        'cMailEID
+        '
+        Me.cMailEID.HeaderText = "E-Mail EID"
+        Me.cMailEID.Name = "cMailEID"
+        Me.cMailEID.ReadOnly = True
+        Me.cMailEID.Visible = False
+        Me.cMailEID.Width = 5
+        '
+        'cOrdner
+        '
+        DataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.cOrdner.DefaultCellStyle = DataGridViewCellStyle2
+        Me.cOrdner.HeaderText = "Ordner"
+        Me.cOrdner.Name = "cOrdner"
+        Me.cOrdner.ReadOnly = True
+        Me.cOrdner.ToolTipText = "Doppelklick um Ordner auszuwählen"
+        Me.cOrdner.Width = 250
+        '
+        'cOrdnerEID
+        '
+        Me.cOrdnerEID.HeaderText = "Ordner EID"
+        Me.cOrdnerEID.Name = "cOrdnerEID"
+        Me.cOrdnerEID.ReadOnly = True
+        Me.cOrdnerEID.Visible = False
+        Me.cOrdnerEID.Width = 5
+        '
+        'cOrdnerSID
+        '
+        Me.cOrdnerSID.HeaderText = "Ordner SID"
+        Me.cOrdnerSID.Name = "cOrdnerSID"
+        Me.cOrdnerSID.ReadOnly = True
+        Me.cOrdnerSID.Visible = False
+        Me.cOrdnerSID.Width = 5
+        '
+        'cCopy
+        '
+        DataGridViewCellStyleCheckBoxEnabled.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyleCheckBoxEnabled.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        DataGridViewCellStyleCheckBoxEnabled.NullValue = False
+        DataGridViewCellStyleCheckBoxDisabled.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyleCheckBoxDisabled.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        DataGridViewCellStyleCheckBoxDisabled.NullValue = False
+        DataGridViewCellStyleCheckBoxDisabled.BackColor = Color.Gray
+        Me.cCopy.DefaultCellStyle = DataGridViewCellStyleCheckBoxEnabled
+        Me.cCopy.HeaderText = "Kopieren"
+        Me.cCopy.Name = "cCopy"
+        Me.cCopy.Width = 80
+        '
+        'cIgnore
+        '
+        Me.cIgnore.DefaultCellStyle = DataGridViewCellStyleCheckBoxEnabled
+        Me.cIgnore.HeaderText = "Ignorieren"
+        Me.cIgnore.Name = "cIgnore"
+        Me.cIgnore.Width = 80
+        '
+        'cMailAdress
+        '
+        Me.cMailAdress.HeaderText = "E-Mail Adresse"
+        Me.cMailAdress.Name = "cMailAdress"
+        Me.cMailAdress.ReadOnly = True
+        Me.cMailAdress.Width = 5
+        '
+        'frmMails
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(241, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(873, 425)
+        Me.Controls.Add(Me.dgMails)
+        Me.Controls.Add(Me.Panel1)
+        Me.DoubleBuffered = True
+        Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmMails"
+        Me.Text = "KdMails.net - Kundenmails kopieren - Übersicht"
+        CType(Me.dgMails, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents dgMails As System.Windows.Forms.DataGridView
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents btnAbbrechen As System.Windows.Forms.Button
+    Friend WithEvents btnKopieren As System.Windows.Forms.Button
+    Friend WithEvents cEA As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents cMail As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents cMailEID As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents cOrdner As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents cOrdnerEID As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents cOrdnerSID As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents cCopy As System.Windows.Forms.DataGridViewCheckBoxColumn
+    Friend WithEvents cIgnore As System.Windows.Forms.DataGridViewCheckBoxColumn
+    Friend WithEvents cMailAdress As System.Windows.Forms.DataGridViewTextBoxColumn
+
+End Class
---
+++ work/2.2.0/2.1.4/KdMails.net/Forms/frmOptionen.vb
@@ -0,0 +1,283 @@
+﻿#Region "Imports"
+Imports System.Data.OleDb
+
+#End Region
+
+Public Class frmOptionen
+
+#Region "Deklarationen"
+
+    Private _strTemp As String = ""
+
+    Private _iWidthNormal As Integer = 309
+    Private _iWidthDebug As Integer = 844
+
+    Private _strSQL As String = "Select I.INTADR_FELD02 KUNDE, SUBSTR(T.TEL_RUFNR, INSTR(T.TEL_RUFNR,'@')) MAIL" & vbNewLine & _
+                               "from S_INTADR I, S_ANSP P, S_TEL T" & vbNewLine & _
+                               "where(I.FIRM_NR = P.FIRM_NR)" & vbNewLine & _
+                               "and I.INTADR_NR=P.INTADR_NR" & vbNewLine & _
+                               "and NOT I.INTADR_FELD02='RS'" & vbNewLine & _
+                               "and P.FIRM_NR=T.FIRM_NR" & vbNewLine & _
+                               "and P.INTADR_NR=T.INTADR_NR" & vbNewLine & _
+                               "and P.ANSP_NR=T.ANSP_NR" & vbNewLine & _
+                               "and P.ANSP_STATUS=0" & vbNewLine & _
+                               "and T.TEL_CODE like 'E-MAIL%'" & vbNewLine & _
+                               "and NOT I.INTADR_FELD02 IS NULL" & vbNewLine & _
+                               "group by I.INTADR_FELD02, SUBSTR(T.TEL_RUFNR, INSTR(T.TEL_RUFNR,'@'))" & vbNewLine & _
+                               "order by 1,2"
+
+#End Region
+
+#Region "Sub"
+    Private Sub frmMails_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Me.Width = _iWidthDebug
+
+        btnDebug_Click(sender, e)
+
+        'Einstellung für Auto Update lesen
+        mdlHaupt.Lese_Registry("Update_Pfad", txtUpdatePfad.Text)
+
+        'Einstellungen einlesen
+        txtServer.Text = frmHaupt.cConfig.GetSettings(clsConfig.ESettings.DB_Server)
+        txtDatenbank.Text = frmHaupt.cConfig.GetSettings(clsConfig.ESettings.DB_Datenbank)
+        txtBenutzer.Text = frmHaupt.cConfig.GetSettings(clsConfig.ESettings.DB_Benutzer)
+        txtKennwort.Text = frmHaupt.cConfig.GetSettings(clsConfig.ESettings.DB_Kennwort)
+
+        txtLDAP.Text = frmHaupt.cConfig.GetSettings(clsConfig.ESettings.LDAP_Domain)
+
+        txtOrdnerEingang.Text = frmHaupt.cConfig.GetSettings(clsConfig.ESettings.Ordner_Eingang, 1)
+        txtOrdnerEingangEID.Text = frmHaupt.cConfig.GetSettings(clsConfig.ESettings.Ordner_Eingang, 2)
+        txtOrdnerEingangSID.Text = frmHaupt.cConfig.GetSettings(clsConfig.ESettings.Ordner_Eingang, 3)
+
+        txtOrdnerAusgang.Text = frmHaupt.cConfig.GetSettings(clsConfig.ESettings.Ordner_Gesendet, 1)
+        txtOrdnerAusgangEID.Text = frmHaupt.cConfig.GetSettings(clsConfig.ESettings.Ordner_Gesendet, 2)
+        txtOrdnerAusgangSID.Text = frmHaupt.cConfig.GetSettings(clsConfig.ESettings.Ordner_Gesendet, 3)
+
+        txtOrdnerZiel.Text = frmHaupt.cConfig.GetSettings(clsConfig.ESettings.Ordner_Ziel, 1)
+        txtOrdnerZielEID.Text = frmHaupt.cConfig.GetSettings(clsConfig.ESettings.Ordner_Ziel, 2)
+        txtOrdnerZielSID.Text = frmHaupt.cConfig.GetSettings(clsConfig.ESettings.Ordner_Ziel, 3)
+
+        If frmHaupt.cConfig.GetSettings(clsConfig.ESettings.Mail_KopierenVerschieben) = 0 Then
+            optKopieren.Checked = True
+        ElseIf frmHaupt.cConfig.GetSettings(clsConfig.ESettings.Mail_KopierenVerschieben) = 1 Then
+            optVerschieben.Checked = True
+        End If
+
+        btnMailDom.Enabled = frmHaupt.cDatenbank.Status
+
+        dgIgnoreList.AllowUserToAddRows = False
+
+        FillDGFolder()
+        FillDGDomain()
+        FillDGIgnoreList()
+
+        dgIgnoreList.AllowUserToAddRows = True
+    End Sub
+
+#End Region
+
+#Region "Funktionen"
+    Private Function FillDGFolder() As Boolean
+        'DataGrid Ordner leeren
+        Do While dgOrdner.Rows.Count > 0
+            dgOrdner.Rows.Remove(dgOrdner.Rows(0))
+        Loop
+
+        For i As Integer = 0 To frmHaupt.cConfig.oOrdnerL.Count - 1
+            dgOrdner.Rows.Add()
+            frmHaupt.cConfig.GetFolder(i, dgOrdner.Rows(i).Cells("cOrdnerName").Value, dgOrdner.Rows(i).Cells("cOrdnerEID").Value, dgOrdner.Rows(i).Cells("cOrdnerSID").Value)
+        Next i
+    End Function
+
+    Private Function FillDGDomain() As Boolean
+        'DataGrid Domänen leeren
+        Do While dgDomain.Rows.Count > 0
+            dgDomain.Rows.Remove(dgDomain.Rows(0))
+        Loop
+
+        For i As Integer = 0 To frmHaupt.cConfig.oDomänenL.Count - 1
+            dgDomain.Rows.Add()
+            frmHaupt.cConfig.GetDomain(i, dgDomain.Rows(i).Cells("cDomäne").Value, dgDomain.Rows(i).Cells("cKunde").Value, dgDomain.Rows(i).Cells("cOutlookOrdner").Value, dgDomain.Rows(i).Cells("cOutlookEID").Value, dgDomain.Rows(i).Cells("cOutlookSID").Value)
+        Next
+    End Function
+
+    Private Function FillDGIgnoreList() As Boolean
+        'DataGrid IgnoreList leeren
+        Do While dgIgnoreList.Rows.Count > 0
+            dgIgnoreList.Rows.Remove(dgIgnoreList.Rows(0))
+        Loop
+
+        For i As Integer = 0 To frmHaupt.cConfig.oIgnoreL.Count - 1
+            dgIgnoreList.Rows.Add()
+            frmHaupt.cConfig.GetIgnoreList(i, dgIgnoreList.Rows(i).Cells("cIgnoreDomäne").Value)
+        Next
+    End Function
+
+    Private Function AddDGToIgnoreList() As Boolean
+
+        For i As Integer = 0 To frmHaupt.cConfig.oIgnoreL.Count - 1
+            frmHaupt.cConfig.RemoveIgnoreList(0)
+        Next
+
+        For i As Integer = 0 To dgIgnoreList.Rows.Count - 1
+            frmHaupt.cConfig.AddIgnoreList(dgIgnoreList.Rows(i).Cells("cIgnoreDomäne").Value)
+        Next
+    End Function
+
+#End Region
+
+#Region "Buttons"
+#Region "Standard"
+    Private Sub btnSpeichern_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSpeichern.Click
+        'Einstellungen Speichern
+        frmHaupt.cConfig.SetSettings(clsConfig.ESettings.DB_Server, txtServer.Text)
+        frmHaupt.cConfig.SetSettings(clsConfig.ESettings.DB_Datenbank, txtDatenbank.Text)
+        frmHaupt.cConfig.SetSettings(clsConfig.ESettings.DB_Benutzer, txtBenutzer.Text)
+        frmHaupt.cConfig.SetSettings(clsConfig.ESettings.DB_Kennwort, txtKennwort.Text)
+        frmHaupt.cConfig.SetSettings(clsConfig.ESettings.LDAP_Domain, txtLDAP.Text)
+        frmHaupt.cConfig.SetSettings(clsConfig.ESettings.Ordner_Eingang, txtOrdnerEingang.Text, txtOrdnerEingangEID.Text, txtOrdnerEingangSID.Text)
+        frmHaupt.cConfig.SetSettings(clsConfig.ESettings.Ordner_Gesendet, txtOrdnerAusgang.Text, txtOrdnerAusgangEID.Text, txtOrdnerAusgangSID.Text)
+        frmHaupt.cConfig.SetSettings(clsConfig.ESettings.Ordner_Ziel, txtOrdnerZiel.Text, txtOrdnerZielEID.Text, txtOrdnerZielSID.Text)
+        If optKopieren.Checked = True Then
+            frmHaupt.cConfig.SetSettings(clsConfig.ESettings.Mail_KopierenVerschieben, 0)
+        ElseIf optVerschieben.Checked = True Then
+            frmHaupt.cConfig.SetSettings(clsConfig.ESettings.Mail_KopierenVerschieben, 1)
+        End If
+
+        AddDGToIgnoreList()
+
+        frmHaupt.cConfig.WriteConfigIgnore()
+        frmHaupt.cConfig.WriteConfigDomain()
+        frmHaupt.cConfig.WriteConfigFolder()
+        frmHaupt.cConfig.WriteConfigSettings()
+    End Sub
+
+    Private Sub btnAbbrechen_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAbbrechen.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnDebug_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDebug.Click
+        If Me.Width = _iWidthNormal Then
+            Me.Width = _iWidthDebug
+            dgDomain.Visible = True
+            dgOrdner.Visible = True
+            btnMarkMails.Enabled = True
+        Else
+            Me.Width = _iWidthNormal
+            dgDomain.Visible = False
+            dgOrdner.Visible = False
+            btnMarkMails.Enabled = False
+        End If
+
+    End Sub
+
+#End Region
+
+#Region "Outlook-Ordner"
+    Private Sub btnOrdnerEingang_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOrdnerEingang.Click
+        'Outlook Ordner auswählen
+        frmHaupt.cOutlook.FolderSelection(txtOrdnerEingang.Text, _
+                                          txtOrdnerEingangEID.Text, _
+                                          txtOrdnerEingangSID.Text)
+    End Sub
+
+    Private Sub btnOrdnerAusgang_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOrdnerAusgang.Click
+        'Outlook Ordner auswählen
+        frmHaupt.cOutlook.FolderSelection(txtOrdnerAusgang.Text, _
+                                          txtOrdnerAusgangEID.Text, _
+                                          txtOrdnerAusgangSID.Text)
+
+    End Sub
+
+    Private Sub btnOrdnerZiel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOrdnerZiel.Click
+        'Outlook Ordner auswählen
+        frmHaupt.cOutlook.FolderSelection(txtOrdnerZiel.Text, _
+                                          txtOrdnerZielEID.Text, _
+                                          txtOrdnerZielSID.Text)
+
+    End Sub
+
+#End Region
+
+#Region "Funktionen"
+    Private Sub btnOrdnerDB_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOrdnerDB.Click
+        Cursor.Current = Cursors.WaitCursor
+
+        If Not txtOrdnerZielEID.Text = "00" Then
+
+            frmHaupt.cOutlook.oOrdner = frmHaupt.cOutlook.GetFolders(txtOrdnerZielEID.Text, txtOrdnerZielSID.Text)
+
+            Do While Not frmHaupt.cOutlook.oOrdner Is Nothing
+                frmHaupt.cConfig.AddFolder(frmHaupt.cOutlook.oOrdner.Name, frmHaupt.cOutlook.oOrdner.EntryID, frmHaupt.cOutlook.oOrdner.StoreID)
+                frmHaupt.cConfig.AddFolderToDomain(frmHaupt.cOutlook.oOrdner.Name, frmHaupt.cOutlook.oOrdner.EntryID, frmHaupt.cOutlook.oOrdner.StoreID)
+                frmHaupt.cOutlook.oOrdner = frmHaupt.cOutlook.GetNextFolder
+            Loop
+
+            FillDGDomain()
+            FillDGFolder()
+
+        End If
+
+        Cursor.Current = Cursors.Default
+    End Sub
+
+    Private Sub btnMailDom_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnMailDom.Click
+
+        Cursor.Current = Cursors.WaitCursor
+
+        frmHaupt.cDatenbank.oReader = frmHaupt.cDatenbank.OpenRS(_strSQL)
+
+        If Not frmHaupt.cDatenbank.oReader Is Nothing Then
+
+
+            While frmHaupt.cDatenbank.oReader.Read
+                frmHaupt.cConfig.AddDomain(frmHaupt.cDatenbank.oReader.GetValue(1).ToString, frmHaupt.cDatenbank.oReader.GetValue(0).ToString, Nothing, Nothing, Nothing)
+
+            End While
+
+            FillDGDomain()
+        End If
+
+        Cursor.Current = Cursors.Default
+    End Sub
+
+    Private Sub btnMarkMails_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnMarkMails.Click
+        Cursor.Current = Cursors.WaitCursor
+        frmHaupt.cOutlook.ResetAllMails(txtOrdnerZielEID.Text, txtOrdnerZielSID.Text)
+        Cursor.Current = Cursors.Default
+    End Sub
+
+    Private Sub btnUpdate_Pfad_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnUpdatePfad.Click
+        mdlHaupt._FolderBrowser.ShowDialog()
+        txtUpdatePfad.Text = mdlHaupt._FolderBrowser.SelectedPath
+        mdlHaupt.Schreibe_Registry("Update_Pfad", txtUpdatePfad.Text)
+    End Sub
+
+#End Region
+
+#End Region
+
+#Region "DebugMode"
+
+    Private Sub dgDomain_CellDoubleClick(ByVal sender As Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles dgDomain.CellDoubleClick
+        MsgBox("Inhalt des Feldes: " & vbNewLine & dgDomain.SelectedCells.Item(0).Value & vbNewLine & vbNewLine & _
+               "Inhalt der Zeile:" & vbNewLine & _
+               "   Domäne: " & dgDomain.Rows(dgDomain.SelectedCells.Item(0).RowIndex).Cells("cDomäne").Value & vbNewLine & _
+               "   Kunden: " & dgDomain.Rows(dgDomain.SelectedCells.Item(0).RowIndex).Cells("cKunde").Value & vbNewLine & _
+               "   Ordern: " & dgDomain.Rows(dgDomain.SelectedCells.Item(0).RowIndex).Cells("cOutlookOrdner").Value & vbNewLine & _
+               "   EID: " & dgDomain.Rows(dgDomain.SelectedCells.Item(0).RowIndex).Cells("cOutlookEID").Value & vbNewLine & _
+               "   SID wird nur bei Doppelklick auf die Spalte angezeigt", MsgBoxStyle.Information, "KdMails.net - Domänen")
+    End Sub
+
+    Private Sub dgOrdner_CellDoubleClick(ByVal sender As Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles dgOrdner.CellDoubleClick
+        MsgBox("Inhalt des Feldes: " & vbNewLine & dgOrdner.SelectedCells.Item(0).Value & vbNewLine & vbNewLine & _
+               "Inhalt der Zeile:" & vbNewLine & _
+               "   Name: " & dgOrdner.Rows(dgOrdner.SelectedCells.Item(0).RowIndex).Cells("cOrdnerName").Value & vbNewLine & _
+               "   EID: " & dgOrdner.Rows(dgOrdner.SelectedCells.Item(0).RowIndex).Cells("cOrdnerEID").Value & vbNewLine & _
+               "   SID wird nur bei Doppelklick auf die Spalte angezeigt", MsgBoxStyle.Information, "KdMails.net - Ordner")
+    End Sub
+
+#End Region
+
+End Class
