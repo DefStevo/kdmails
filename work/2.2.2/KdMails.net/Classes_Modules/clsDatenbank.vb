ab2fe2c0dcab87c5b7bc1cfbaf2c34d410161819
DefStevo@gmail.com <DefStevo@gmail.com@1bb4064d-4001-7b1c-8927-4fcd3d59ee48>
2015-07-07 19:15:40 +0000

    [KdMails.net] v 2.2.x Beginn Version 2.2.2
    [KdMails.net.Update] v 1.0.2
    - NONE

---
+++ work/2.2.2/KdMails.net/Classes_Modules/clsDatenbank.vb
@@ -0,0 +1,168 @@
+﻿#Region "Imports"
+Imports System.Data.OleDb
+
+#End Region
+
+Public Class clsDatenbank
+
+#Region "Deklarationen"
+    Private _oraConn As OleDbConnection
+    Private _oraCommand As OleDbCommand
+    Private _oraDataReader As OleDbDataReader
+    Private _bConnStatus As Boolean = False
+
+    Private _strServer As String
+    Private _strDB As String
+    Private _strUser As String
+    Private _strPass As String
+    Private _strSQL As String
+
+    Public oReader As OleDbDataReader
+#End Region
+
+#Region "Properties"
+    Property Server() As String
+        Get
+            Return _strServer
+        End Get
+        Set(ByVal value As String)
+            _strServer = value
+        End Set
+    End Property
+
+    Property DB() As String
+        Get
+            Return _strDB
+        End Get
+        Set(ByVal value As String)
+            _strDB = value
+        End Set
+    End Property
+
+    Property User() As String
+        Get
+            Return _strUser
+        End Get
+        Set(ByVal value As String)
+            _strUser = value
+        End Set
+    End Property
+
+    Property Pass() As String
+        Get
+            Return _strPass
+        End Get
+        Set(ByVal value As String)
+            _strPass = value
+        End Set
+    End Property
+
+    ReadOnly Property RS() As OleDbDataReader
+        Get
+            Return _oraDataReader
+        End Get
+    End Property
+
+    ReadOnly Property Status As Boolean
+        Get
+            Return _bConnStatus
+        End Get
+    End Property
+
+#End Region
+
+#Region "Funktionen"
+    Function SetConnectionInfo(ByVal Server As String, ByVal Datenbank As String, ByVal Benutzer As String, ByVal Kennwort As String) As Boolean
+        'Prüfen ob die Verbindung bereits besteht
+        If _bConnStatus = True Then
+            Return False
+        End If
+        _strServer = Server
+        _strDB = Datenbank
+        _strUser = Benutzer
+        _strPass = Kennwort
+        _bConnStatus = False
+        Return True
+    End Function
+
+    Function GetConnectionInfo(ByRef Server As String, ByRef Datenbank As String, ByRef Benutzer As String, ByRef Kennwort As String, ByRef Status As Boolean) As Boolean
+        Server = _strServer
+        Datenbank = _strDB
+        Benutzer = _strUser
+        Kennwort = _strPass
+        Status = _bConnStatus
+    End Function
+
+    Function ConnectDB() As String
+        'Connection Status prüfen ob Verbindung bereits besteht
+        If _bConnStatus = True Then
+            DisconnectDB()
+        End If
+
+        'Prüfen ob alle Parameter vorhanden sind
+        If _strServer = "" Or _strDB = "" Or _strUser = "" Or _strPass = "" Then
+            Return False
+        End If
+
+        _oraConn = New OleDbConnection("Provider=MSDAORA.1" & _
+                                       ";Data Source=" & _strServer & _
+                                       ";Database=" & _strDB & _
+                                       ";User ID=" & _strUser & _
+                                       ";Password=" & _strPass)
+        Try
+            _oraConn.Open()
+        Catch ex As System.Data.OleDb.OleDbException
+            If Not mdlHaupt._Batch Then
+                Select Case ex.ErrorCode
+                    Case -2147467259
+                        'Provider nicht installiert
+
+                    Case Else
+                        MsgBox("Fehler: " & ex.ToString & vbCrLf & vbCrLf & _
+                       "Server: " & _strServer & vbCrLf & _
+                       "Datebank: " & _strDB & vbCrLf & _
+                       "Benutzer: " & _strUser, MsgBoxStyle.Critical, "Fehler bei der Verbindung zur Datenbank")
+                End Select
+            End If
+            _bConnStatus = False
+            Return "Keine Verbindung zur Datenbank " & _strDB & "@" & _strServer
+
+        End Try
+
+        _bConnStatus = True
+        Return "Verbunden mit Datenbank " & _strDB & "@" & _strServer
+    End Function
+
+    Function DisconnectDB() As Boolean
+        If _bConnStatus = True Then
+            _oraConn.Close()
+            _oraCommand = Nothing
+            _oraDataReader = Nothing
+            _oraConn = Nothing
+            _strServer = Nothing
+            _strDB = Nothing
+            _strUser = Nothing
+            _strPass = Nothing
+            _strSQL = Nothing
+            _bConnStatus = False
+            Return True
+        End If
+    End Function
+
+    Function OpenRS(ByVal SQL As String) As OleDbDataReader
+        If _bConnStatus = True Then
+            _oraCommand = New OleDbCommand(SQL, _oraConn)
+            Return _oraCommand.ExecuteReader
+        Else
+            ConnectDB()
+            If _bConnStatus = False Then
+                Return Nothing
+            Else
+                Return OpenRS(SQL)
+            End If
+        End If
+    End Function
+
+#End Region
+
+End Class
---
+++ work/2.2.2/KdMails.net/Forms/frmHaupt.vb
@@ -0,0 +1,199 @@
+﻿#Region "Imports"
+Imports KdMails.net.clsOutlook
+Imports KdMails.net.clsDatenbank
+Imports KdMails.net.clsConfig
+Imports KdMails.net.clsLDAP
+
+#End Region
+
+Public Class frmHaupt
+
+#Region "Deklarationen"
+    Public Shared cOutlook As New clsOutlook
+    Public Shared cDatenbank As New clsDatenbank
+    Public Shared cConfig As New clsConfig
+    Public Shared cLDAP As New clsLDAP
+    Public Shared cLogging As New clsLogging
+    Public Shared cLoggingRead As New clsLogging
+
+    Public Shared sHDRLog As New clsLogging.sLogHDRInfos
+
+    Public strDate As String = Format(Now, "dd.MM.yyyy")
+    Public strTime As String = Format(Now, "HH:mm")
+    Public strLogFile As String = Format(Now, "yyyyMMdd_HHmmss") & ".log"
+
+#End Region
+
+#Region "Subs"
+    Private Sub frmHaupt_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.Text = Me.Text & _
+                  " (v. " & _
+                  My.Application.Info.Version.Major.ToString & "." & _
+                  My.Application.Info.Version.Minor.ToString & "." & _
+                  My.Application.Info.Version.Build.ToString & ")"
+
+        Timer1.Interval = 1
+        Timer1.Enabled = True
+
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        'Fenster im Batchmodus nicht anzeigen
+        If mdlHaupt._Batch Then
+            Me.Visible = False
+        End If
+
+        Timer1.Enabled = False
+
+        Cursor.Current = Cursors.WaitCursor
+        Me.Focus()
+
+        'Standardwerte für Logging setzen
+        sHDRLog.SetInfo(clsLogging.ELogHDRInfos.Anzahl_Ungelesen, "0")
+        sHDRLog.SetInfo(clsLogging.ELogHDRInfos.Anzahl_Ungelesen_Neu, "0")
+        sHDRLog.SetInfo(clsLogging.ELogHDRInfos.Anzahl_Posteingang, "0")
+        sHDRLog.SetInfo(clsLogging.ELogHDRInfos.Anzahl_Postausgang, "0")
+        sHDRLog.SetInfo(clsLogging.ELogHDRInfos.Letzter_Lauf, "Kein Lauf durchgeführt")
+        sHDRLog.SetInfo(clsLogging.ELogHDRInfos.Aktueller_Lauf, "Kein Lauf durchgeführt")
+        sHDRLog.SetInfo(clsLogging.ELogHDRInfos.Version, _
+                  My.Application.Info.Version.Major.ToString & "." & _
+                  My.Application.Info.Version.Minor.ToString & "." & _
+                  My.Application.Info.Version.Build.ToString)
+
+        'Einstellung lesen (Settings, IgnoreList, Domänen, Ordner)
+        My.Settings.Upgrade()
+        cConfig.InitConfig(True, True, True, True, True)
+
+        InitLog()
+
+        InitOutlook()
+
+        InitLDAP()
+
+        InitDB()
+
+        'Status anzeigen
+        stStatus.Text = "|Letzer Lauf: " + cConfig.GetSettings(ESettings.LetzerLauf_Datum) + " " + cConfig.GetSettings(ESettings.LetzerLauf_Zeit)
+
+        sHDRLog.SetInfo(clsLogging.ELogHDRInfos.Letzter_Lauf, cConfig.GetSettings(ESettings.LetzerLauf_Datum) + " " + cConfig.GetSettings(ESettings.LetzerLauf_Zeit))
+
+        'Fertig (Fenster freigeben)
+        btnLos.Enabled = True
+        btnOptionen.Enabled = True
+        btnProtokoll.Enabled = True
+        Cursor.Current = Cursors.Default
+
+        If mdlHaupt._Batch Then
+            btnVerschieben_Click(sender, e)
+            Me.Close()
+        End If
+    End Sub
+
+    Sub InitLog()
+        'Logging Initialisieren
+        cLogging.InitLOG()
+
+    End Sub
+
+    Sub InitOutlook()
+        'Outlook Initialisieren
+        stOutlook.Image = My.Resources.Status_CO
+        Me.Refresh()
+        cOutlook.InitOutlook()
+        If cOutlook.Status = True Then
+            stOutlook.Image = My.Resources.Status_OK
+        Else
+            stOutlook.Image = My.Resources.Status_NA
+        End If
+        Me.Refresh()
+    End Sub
+
+    Sub InitLDAP()
+        'Verbindung zu LDAP herstellen
+        If Not cConfig.GetSettings(ESettings.LDAP_Domain) = Nothing Then
+            stLDAP.Image = My.Resources.Status_CO
+            Me.Refresh()
+
+            cLDAP.InitLDAP(cConfig.GetSettings(ESettings.LDAP_Domain), cConfig.GetSettings(ESettings.LDAP_Benutzer), cConfig.GetSettings(ESettings.LDAP_Kennwort))
+
+            If cLDAP.Status = True Then
+                stLDAP.Image = My.Resources.Status_OK
+            Else
+                stLDAP.Image = My.Resources.Status_NA()
+            End If
+
+            Me.Refresh()
+        End If
+    End Sub
+
+    Sub InitDB()
+        'Verbindung zu Datenbank herstellen
+        stDB.Image = My.Resources.Status_CO
+        Me.Refresh()
+        cDatenbank.SetConnectionInfo(cConfig.GetSettings(ESettings.DB_Server), _
+                                     cConfig.GetSettings(ESettings.DB_Datenbank), _
+                                     cConfig.GetSettings(ESettings.DB_Benutzer), _
+                                     cConfig.GetSettings(ESettings.DB_Kennwort))
+        cDatenbank.ConnectDB()
+
+        If cDatenbank.Status = True Then
+            stDB.Image = My.Resources.Status_OK
+        Else
+            stDB.Image = My.Resources.Status_NA
+        End If
+        Me.Refresh()
+    End Sub
+
+    Private Sub frmHaupt_FormClosed(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        cOutlook.UnloadOutlook()
+        cDatenbank.DisconnectDB()
+        cConfig.WriteConfigFolder()
+        cConfig.WriteConfigDomain()
+        cConfig.UnloadConfig()
+        Application.Exit()
+    End Sub
+
+    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles picMail.Click
+        frmChangeLog.ShowDialog()
+    End Sub
+
+#End Region
+
+#Region "Buttons"
+    Private Sub btnVerschieben_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLos.Click
+        cLoggingRead = New clsLogging
+        frmMails.Protokoll = False
+        frmMails.strProtokoll = ""
+        frmMails.ShowDialog()
+    End Sub
+
+    Private Sub btnOptionen_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOptionen.Click
+        frmOptionen.ShowDialog()
+
+        Cursor.Current = Cursors.WaitCursor
+        If Not cConfig.GetSettings(ESettings.LDAP_Domain) = cLDAP.Domäne Then
+            InitLDAP()
+        End If
+
+        If cConfig.GetSettings(ESettings.DB_Server) <> cDatenbank.Server Or _
+        cConfig.GetSettings(ESettings.DB_Datenbank) <> cDatenbank.DB Or _
+        cConfig.GetSettings(ESettings.DB_Benutzer) <> cDatenbank.User Or _
+        cConfig.GetSettings(ESettings.DB_Kennwort) <> cDatenbank.Pass Then
+            InitDB()
+        End If
+        Cursor.Current = Cursors.Default
+
+    End Sub
+
+    Private Sub btnProtokoll_Click(sender As System.Object, e As System.EventArgs) Handles btnProtokoll.Click
+        cLoggingRead = New clsLogging
+        frmMails.Protokoll = True
+        frmMails.strProtokoll = ""
+        frmMails.ShowDialog()
+    End Sub
+
+#End Region
+
+
+
+End Class
